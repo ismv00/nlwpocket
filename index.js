e5e4c92975541b78c86be7e6dc779199e5c86203
ismv00 <euigoorr@MacBook-Pro-de-Igor.local>
2024-09-10 23:06:38 -0400

    feat:first-commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let message = "Hello Igor";
+message = "Hello Igor 2";
+
+let metas = ["Igor", "Meta 2", "meta 3"];
+
+let meta = {
+  value: "Ler um livro por mês",
+  checked: false,
+};
+
+console.log(message);
+console.log(metas[1]);
+console.log(meta.value);
